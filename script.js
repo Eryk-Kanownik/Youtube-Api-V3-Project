ab82ef80ccd840d8884f438be416fb10ef060d9e
Eryk <eryk.patryk.kanownik@gmail.com>
2020-05-25 20:57:41 +0200

    ready

--- script.js
+++ script.js
@@ -33,8 +33,15 @@ const getData = () => {
 }
 
 const containerCreator = (data) => {
+    let videoCard = document.createElement("div");
+    videoCard.className = "video-card"
+    videoCard.addEventListener('click',() => {
+        console.log("hello")
+        isModalActive = true;
+        createModal(data.id.videoId,data.snippet.title,data.snippet.channelId,data.snippet.channelTitle)
+    })
     let container = 
-    `<div class='video-card'>
+    `
         <div class='thumbnail'>
             <img src="${data.snippet.thumbnails.medium.url}">
         </div>
@@ -42,6 +49,35 @@ const containerCreator = (data) => {
             <div class="title"><a href="https://www.youtube.com/watch?v=${data.id.videoId}" target="blank">${data.snippet.title}</a></div>
             <div class="channelName"><a href="https://www.youtube.com/channel/${data.snippet.channelId}" target="blank">${data.snippet.channelTitle}</a></div>
         </div>
-    </div>`
-    grid.innerHTML += container;
+    `
+    videoCard.innerHTML = container;
+    console.log(videoCard)
+    grid.appendChild(videoCard);
+}
+
+const createModal = (vidId,vidTitle,channelId,channelTitle) => {
+    let modalElement = document.createElement("div");
+    modalElement.className = "modal";
+    modalElement.style.display = "flex"
+
+    modalElement.addEventListener("click", (e) => {
+        if(e.target === modalElement){
+            modalElement.style.display = "none"
+        }
+    })
+    let modal = `
+        <div class="window">
+            <object data="http://www.youtube.com/embed/${vidId}"></object>
+            <div class="info">
+                
+                <div>
+                    <p><a href="https://www.youtube.com/channel/${channelId}" target="blank">${channelTitle}</a></p>
+                    <p><a href="https://www.youtube.com/watch?v=${vidId}" target="blank">${vidTitle}</a></p>
+                    <p class="inf">Remember to pause video before you click outside</p>
+                </div>
+            </div>
+        </div>
+    `
+    modalElement.innerHTML = modal
+    document.body.appendChild(modalElement);
 }
